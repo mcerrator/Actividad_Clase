7c6f6c79d6f406c3fa00d87bc53a8103750d81e6
Miguel Cerrato <miguelcerrato32@gmail.com>
2026-09-17 19:45:30 -0600

    cambio de datos de conexion

--- BE-FE/api-usuario/app.py
+++ BE-FE/api-usuario/app.py
@@ -12,9 +12,9 @@ CORS(app, resources={
     }
 })
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'acela.proxy.rlwy.net'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Admin1234'
+app.config['MYSQL_PASSWORD'] = 'cQJVUszYhFdbCtUczEllzexdUJFAvoFo'
 app.config['MYSQL_DB'] = 'VENTAS'
 
 db = ConexionDB(
