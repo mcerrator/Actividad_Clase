3565603526a0de0bbc0f52870175e0663ab84add
Miguel Cerrato <miguelcerrato32@gmail.com>
2026-09-17 20:45:48 -0600

    ingreso de archivos

--- conexion.py
+++ conexion.py
@@ -7,6 +7,7 @@ class ConexionDB:
         self.password = password
         self.db = db
         self.conexion = None
+        self.port = 3306
 
     def conectar(self):
         if self.conexion is None or not self.conexion.open:
